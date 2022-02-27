077c5cbc152a944154ac0b9da3ab34d6fa75129a
Lucas <lucas.bellissimo@gmail.com>
2022-02-27 16:16:12 +0100

    Initial commit

---
+++ src/test/java/com/lbel/example/grpcspring/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbel.example.grpcspring.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
